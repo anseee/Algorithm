1bc02a2a9d2886c6493c1e0b8a7f916b73e0d522
anseee <parksungwon1113@gmail.com>
2022-06-15 01:33:01 +0900

    [*][+] 2017 카카오 알고리즘 문제 정리중.

---
+++ Algorithms/Algorithms/Kakao/Kakao2017Algorithms.swift
@@ -0,0 +1,25 @@
+//
+//  Kakao2017Algorithms.swift
+//  Algorithms
+//
+//  Created by 박성원 on 2022/06/15.
+//
+
+import Foundation
+
+struct Kakao2017Algorithms {
+    static func secretMap(n: Int, arr1: [Int], arr2: [Int]) -> [String] {
+        var results: [String] = []
+        
+        for i in 0..<n {
+            let arr1Value = arr1[i]
+            let arr2Value = arr2[i]
+            var result = String(arr1Value | arr2Value, radix: 2)
+            result = result.replacingOccurrences(of: "0", with: "+")
+            result = result.replacingOccurrences(of: "1", with: "#")
+            results.append(result)
+        }
+
+        return results
+    }
+}
---
+++ Algorithms/AlgorithmsTests/Kakao/Kakao2017AlgorithmsTests.swift
@@ -0,0 +1,37 @@
+//
+//  Kakao2017AlgorithmsTests.swift
+//  AlgorithmsTests
+//
+//  Created by 박성원 on 2022/06/15.
+//
+
+import XCTest
+@testable import Algorithms
+
+class Kakao2017AlgorithmsTests: XCTestCase {
+    func testSecretMap() throws {
+        let n = 5
+        let arr1 = [9, 20, 28, 18, 11]
+        let arr2 = [30, 1, 21, 17, 28]
+        let expectResult = ["#####","# # #", "### #", "# ##", "#####"]
+        
+        XCTAssert(1 <= n && n <= 16)
+        XCTAssertEqual(n, arr1.count, "arr1은 길이 n인 정수 배열로 주어진다.")
+        XCTAssertEqual(n, arr2.count, "arr2는 길이 n인 정수 배열로 주어진다.")
+        
+        arr1.forEach { value in
+            XCTAssert(0 <= value && value <= Int(pow(2.0, Float(n)) - 1), "0 ≦ x ≦ 2^n – 1을 만족해야 합니다.")
+        }
+        
+        let result = Kakao2017Algorithms.secretMap(n: n, arr1: arr1, arr2: arr2)
+        
+        XCTAssertEqual(result, expectResult, "기대값과 다릅니다. 알고리즘 결과값: \(result)")
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+}
